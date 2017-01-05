8af26e5ddaa9d6b71a441da9b96b08d9aaae38bb
chenz <chenz@VENKAT-PC.ciotech.com>
2017-01-05 10:40:35 -0800

    Improve Token Auth for new API endpoint

--- upload/catalog/controller/startup/session.php
+++ upload/catalog/controller/startup/session.php
@@ -1,7 +1,8 @@
 <?php
 class ControllerStartupSession extends Controller {
 	public function index() {
-		if (isset($this->request->get['token']) && isset($this->request->get['route']) && in_array(substr($this->request->get['route'], 0, 4), array('api/', 'feed'))) {
+		if (isset($this->request->get['token']) && isset($this->request->get['route']) 
+				&& ( substr($this->request->get['route'], 0, 4) == 'api/' || substr($this->request->get['route'], 0, 13) == 'feed/gss_api/' ) ) {
 			$this->db->query("DELETE FROM `" . DB_PREFIX . "api_session` WHERE TIMESTAMPADD(HOUR, 1, date_modified) < NOW()");
 		
 			$query = $this->db->query("SELECT DISTINCT * FROM `" . DB_PREFIX . "api` `a` LEFT JOIN `" . DB_PREFIX . "api_session` `as` ON (a.api_id = as.api_id) LEFT JOIN " . DB_PREFIX . "api_ip `ai` ON (as.api_id = ai.api_id) WHERE a.status = '1' AND as.token = '" . $this->db->escape($this->request->get['token']) . "' AND ai.ip = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "'");
